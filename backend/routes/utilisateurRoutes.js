c847bd68a8b39e684e25121765ef9ccbfd0935b6
eyaHwess <eyahwess74@gmail.com>
2024-11-08 21:30:56 +0100

    added auth middleware

--- backend/routes/utilisateurRoutes.js
+++ backend/routes/utilisateurRoutes.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const router = express.Router();
 const utilisateurController = require('../controllers/UtilisateurController');
@@ -9,7 +7,7 @@ router.get('/:id', utilisateurController.getUtilisateurById);
 router.get('/id/:firstName/:lastName', utilisateurController.getUserIdByName);
 router.post('/add', utilisateurController.createUtilisateur);
 router.get('/role/:role', utilisateurController.getUtilisateursByRole);
-router.post('/login', utilisateurController.getUserByEmailAndPassword);
+router.post('/login', utilisateurController.login);
 router.post('/change-password', utilisateurController.changePassword);
 router.post('/create', utilisateurController.createUserByAdmin);
 router.delete('/:id', utilisateurController.deleteUtilisateur);
